4848b72102573ce5f59256f6cb46133e42853160
Rodrigo Tocantins <rtocantins.silva@gmail.com>
2020-12-19 13:09:14 -0500

    new file:   api_v1/Dockerfile 	new file:   api_v1/am2302.sh 	new file:   docker-compose.yaml

---
+++ zabbix/api_v1/am2302.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while true;
+do
+cd /tmp
+rm -f /tmp/index.html*
+wget -k 10.20.144.9 #> /dev/null
+if [ $? -eq 0 ];then
+
+export HSOIL=`grep kg  index.html |awk '{print $2}'`
+zabbix_sender -z localhost -s Pimental_Sitio_Paulo -k pH -o $HSOIL
+
+else
+
+echo "ERRO | Falha sensor umidade do solo | `date +%d-%m-%y-%H:%M` " >> /tmp/am2302.log
+
+fi
+
+sleep 300
+
+done
+
+exit 0
+
